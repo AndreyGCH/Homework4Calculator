d30add6672efebfac9b9404c91a47d4afface132
Guillermo Andrey Guevara Chinchilla <guevaraandrey@gmail.com>
2021-02-28 20:15:23 -0800

    Initial Commit

---
+++ Homework4Calculator/Homework4CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  Homework4CalculatorApp.swift
+//  Homework4Calculator
+//
+//  Created by Andrey Guevara on 28/2/21.
+//
+
+import SwiftUI
+
+@main
+struct Homework4CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
